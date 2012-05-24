465b4179017d467bea296b92e672f217cadad90a
Arun <twopieman@gmail.com>
2012-05-23 23:04:05 -0700

    First scratch code for the Picture service
    Currently all of the logic for interacting with the azure blob storage
    is done in the constructor.

---
+++ PictureServiceProject/PictureServiceWebRole/IPictureService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PictureServiceWebRole
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IPictureService
+    {
+
+        [OperationContract]
+        Picture GetPicture(string name);
+
+        [OperationContract]
+        void UploadPicture(Picture picture);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Picture
+    {
+        [DataMember]
+        public byte[] PictureStream { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public List<string> Tags { get; set; }
+    }
+}
---
+++ PictureServiceProject/PictureServiceWebRole/PictureService.svc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.StorageClient;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace PictureServiceWebRole
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class PictureService : IPictureService
+    {
+        public PictureService()
+        {
+            //First create the Container for this service
+            CloudStorageAccount account = CloudStorageAccount.Parse(RoleEnvironment.GetConfigurationSettingValue("StorageConnectionString"));
+            CloudBlobClient client = account.CreateCloudBlobClient();
+            CloudBlobContainer container = client.GetContainerReference("testcontainer");
+            container.CreateIfNotExist();
+            container.SetPermissions(new BlobContainerPermissions() { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            //now create the Blob   
+            CloudBlob blob = container.GetBlobReference("TestBlob");
+
+            using (var fileStream = System.IO.File.OpenRead("C:\\Temp\\test.txt")) 
+            {
+                blob.UploadFromStream(fileStream);
+            }
+
+        }
+
+
+
+        public Picture GetPicture(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UploadPicture(Picture picture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
